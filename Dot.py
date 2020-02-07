f61a400c11d422036e2d7ff41b343764302ff22d
Anik Patel <57848057+Anik-Patel@users.noreply.github.com>
2020-02-06 19:22:40 -0500

    Changes from turtle to pygame

--- Dot.py
+++ Dot.py
@@ -1,88 +1,74 @@
-import numpy as np
+import pygame
 import math
-import random
-import time 
-import turtle
-from fractions import Fraction
+
+GREEN = (20, 255, 140)
+GREY = (210, 210 ,210)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+PURPLE = (255, 0, 255)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+
+
+class Dot(pygame.sprite.Sprite):
+    # This class represents a car. It derives from the "Sprite" class in Pygame.
+
+    def __init__(self, color, width, height):
+        # Call the parent class (Sprite) constructor
+        super().__init__()
+
+        # Pass in the color of the car, and its x and y position, width and height.
+        # Set the background color and set it to be transparent
+        self.image = pygame.Surface([width, height])
+        self.image.fill(WHITE)
+        self.image.set_colorkey(WHITE)
+        self.dead = False
+        self.infected = False
+        self.color = color
+        self.width = width
+        self.height = height
+        self.cured = False
+        self.x = 0
+        self.y = 0
+
+        # Draw the car (a rectangle!)
+        pygame.draw.rect(self.image, self.color, [0, 0, self.width, self.height])
+
+        # Instead we could load a proper pciture of a car...
+        # self.image = pygame.image.load("car.png").convert_alpha()
+
+        # Fetch the rectangle object that has the dimensions of the image.
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+
+    def update_(self):
+        if self.dead == False:
+            if self.infected == True:
+                self.color = RED
+            else:
+                self.color = BLUE
+
+
+        else:
+            self.color = BLACK
+
+        pygame.draw.rect(self.image, self.color, [0, 0, self.width, self.height])
+
+        # Instead we could load a proper pciture of a car...
+        # self.image = pygame.image.load("car.png").convert_alpha()
+
+        # Fetch the rectangle object that has the dimensions of the image.
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
 
 
 
 
-infection_chance = 1
-infection_chance = 100/infection_chance
 
 
-
-
-singular_infection_chance = 1
-singular_infection_chance = 100/infection_chance
-
-curing_chance = 100
-curing_chance = 100/curing_chance
-
-
-
-class Dot():
-	def __init__(self):
-
-		self.infected = False
-		self.cured = False
-		self.dead = False
-
-		
-		if random.randint(0, round(infection_chance)) == 1:
-			self.infected = True
-			
-		else:
-			self.infected = False
-		
-		
-
-
-		self.dot = turtle.Turtle()
-		self.dot.speed(0)
-		self.dot.color('black')
-		self.dot.shape('circle')
-		self.dot.up()
-
-	def change_color(self):
-		if self.dead == False:
-			if self.infected == False:
-				self.dot.color('blue')
-
-			if self.infected == True:
-				self.dot.color('red')
-
-
-
-		if self.dead == True:
-			self.dot.color('black')
-
-
-
-		
-
-
-
-	def move_to(self, x, y):
-		self.dot.goto(x, y)
-
-
-	def infect(self):
-		if self.dead == False:
-			if self.cured == False:
-				if random.randint(0, singular_infection_chance) == 1:
-					self.infected = True
-
-
-	def cure(self, chance):
-		if self.dead == False:
-
-			if random.randint(0, chance) == 1:
-				self.infected = False
-				self.cured = True
-			
-		
 
 
 
